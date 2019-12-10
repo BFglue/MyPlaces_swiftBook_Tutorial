d6fe05eca07bd044e8611668e7f8a40e4797bcf9
Dmitry <dima@kitendo.ru>
2019-12-11 01:51:15 +0500

    Work with Model

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Березин on 11/12/2019.
+//  Copyright © 2019 Dmitry Berezin. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+    
+}
