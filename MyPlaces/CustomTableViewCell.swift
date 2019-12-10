fbfb55e5288313ddc5a2cd38791811880fb3d66d
Dmitry <dima@kitendo.ru>
2019-12-10 18:37:35 +0500

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Березин on 10/12/2019.
+//  Copyright © 2019 Dmitry Berezin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
